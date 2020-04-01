93f3f8f92298a8b8723fe3f1ba1351e76e329916
Brandon Brown <brandonbrown4792@gmail.com>
2020-04-01 00:29:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,17 @@
 class Dog
   @@all = []
   
+  attr_accessor :name
+  
   def initialize
     @@all << self
   end
   
   def self.all
     @@all
+  end
+  
+  def self.print_all
+    
   end
 end
